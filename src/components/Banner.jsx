f8fee4b743f4dc122450564da45f1a04ecd305d3
Aryan Gupta <guptaaryan2050@gmail.com>
2024-08-12 15:51:00 +0530

    change banner height

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Banner = () => {
   return (
     <>
-      <div className="w-[100vw] h-[500px] mx-auto object-cover relative z-0">
+      <div className="w-[100vw] h-[77vh] mx-auto object-cover relative z-0">
        <img src="delhi_bg.webp" alt="" className='w-[100%] h-[100%] object-cover'/>
       </div>
     </>
